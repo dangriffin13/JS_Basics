4bc33cd57ae54c6f9cbe3d4e8ed13e74fb680b9d
dangriffin13 <dangriffin13@gmail.com>
2018-07-06 19:23:55 -0400

    syntax corrections

--- fizz_buzz.js
+++ fizz_buzz.js
@@ -2,8 +2,16 @@
 //40 can be changed to a variable to alter the length of the game
 
 for (var i=1; i < 40; i++){
-    if (i % 5 == 0 and i % 7 == 0)  console.log("FizzBuzz");
-    else if (i % 5 == 0) console.log("Fizz")
-    else if (i % 7 == 0) console.log("Buzz")
-    else console.log(i)
+    if (i % 5 == 0 && i % 7 == 0)  {
+        console.log("FizzBuzz");
+    }
+    else if (i % 5 == 0) {
+        console.log("Fizz");
+    }
+    else if (i % 7 == 0) {
+        console.log("Buzz");
+    }
+    else {
+        console.log(i);
+    }
 }
